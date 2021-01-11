de30b2d35c83cc084245f56122cbc408e727b991
lau.thomsen.privat@gmail.com <lau.thomsen.privat@gmail.com>
2021-01-11 10:55:38 +0100

    Made basic build house function for one house

--- src/main/java/Game/controller/FieldController.java
+++ src/main/java/Game/controller/FieldController.java
@@ -4,7 +4,7 @@ import Game.Model.*;
 import Game.Model.Fields.*;
 
 public class FieldController {
-
+// You have to set position or none of the FieldController Methods will work
     private Field[] fields = {new Start(),new BuildableField(),new ChanceCard(), new BuildableField(), new IncomeTax(), new ShippingLine()
     , new BuildableField(), new ChanceCard(), new BuildableField(), new BuildableField(), new Prison(), new BuildableField(), new Brewery(),
     new BuildableField(), new BuildableField(), new ShippingLine(), new BuildableField(), new ChanceCard(), new BuildableField(),new BuildableField()
@@ -25,6 +25,21 @@ public class FieldController {
             ,0, 220, 0, 220, 240, 200, 260
             ,260, 150, 280, 0, 300, 300, 0, 320
             , 200, 0, 350, 200, 400};
+    private int[] houseCosts = {0,50,0, 50, 0, 0
+            , 50, 0, 50, 50, 0, 100, 0,
+            100, 100, 0, 100, 0, 100,100
+            ,0, 150, 0, 150, 150, 0, 150
+            ,150, 0, 150, 0, 200, 200, 0, 200
+            , 0, 0, 200, 0, 200};
+
+    private int[][] housePrices = {{}, {10,30,90,160,250},{}, {20,60,180,320,540}, {}, {200,200,200,200,200}
+            , {30,90,270,400,550}, {}, {30,90,270,400,550}, {40,100,300,450,600}, {}, {50,150,450,625,750}, {150,150,150,150,150},
+            {50,150,450,625,750} ,{60,180,500,700,900} , {200,200,200,200,200},{70,200,550,750,950} , {},{70,200,550,750,950} ,{80,220,600,800,1000}
+            ,{}, {90,250,700,875,1050}, {}, {90,250,700,875,1050}, {100,300,750,925,1100}, {200,200,200,200,200}, {110,330,800,1150}
+            ,{110,330,800,975,1150}, {}, {120,360,850,1025,1200}, {}, {130,390,900,1100,1275}, {130,390,900,1100,1275}, {}, {150,450,1000,1200,1400}
+            , {200,200,200,200,200}, {}, {175,500,1100,1300,1500}, {}, {200,600,1400,1700,2000}};
+
+    private int[] Houses = new int[40];
 
     private String[] fieldTitles = {"Start","Rødovrevej", "", "Hvidovre", "", "Øresund A/S", "Roskildevej", "", "Valby \n Langgade", "Allégade", "", "Frederiksberg \n Allé", "Tuborg",
             "Bülowsvej", "Gl. Kongevej", "D. F. D. S.", "Bernstoffsvej", "", "Hellerupvej", "Strandvej", "Helle", "Trianglen", "", "Østerbro-\ngade \n", "Grønningen", "Y. K.",
@@ -72,8 +87,28 @@ public class FieldController {
 
     }
 
+
+
     public void setPosition(int position){
         this.position = position;
+    }
+    public void buildHouses(Player player){
+        switch (Houses[position]){
+            case 0: player.playerBalanceUpdate(-houseCosts[position]);
+            Houses[position] = 1;
+        }
+    }
+
+    public int[] getHouseCosts() {
+        return houseCosts;
+    }
+
+    public int[][] getHousePrices() {
+        return housePrices;
+    }
+
+    public int[] getHouses() {
+        return Houses;
     }
 
     public Field[] getFields() {
--- src/main/java/Test/FieldControllerTest.java
+++ src/main/java/Test/FieldControllerTest.java
@@ -1,6 +1,7 @@
 package Test;
 
 import Game.controller.FieldController;
+import Game.controller.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,16 @@ class FieldControllerTest {
 
     @Test
     void getRent2() {
+    }
+
+    @Test
+    void buildHouses() {
+        FieldController field = new FieldController();
+        Player p1 = new Player("Hello");
+        p1.starterScore(1);
+        field.setPosition(1);
+        field.buildHouses(p1);
+        System.out.println(field.getHouses()[1]);
+        System.out.println(p1.playerBalance());
     }
 }
